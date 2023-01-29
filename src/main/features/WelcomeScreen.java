06e9b44241c158133ea7b8e6b5f205d943f50241
unknown <a.gamil.qm@gmail.com>
2023-01-29 14:40:43 +0200

    starting the project

---
+++ src/main/features/WelcomeScreen.java
@@ -0,0 +1,15 @@
+package features;
+
+public class WelcomeScreen {
+    static public void welcome (){
+        System.out.println("Hello ! hope you enjoy using me, i am file Handler Application i will be at your Service :D ");
+        System.out.println("-------------------------    ---------    -----------------------------");
+        System.out.println("-------------------------    ---------    -----------------------------");
+        System.out.println("This Application is used to handle files you can find the option in the user Interface");
+        System.out.println("-------------------------    ---------    -----------------------------");
+        System.out.println("-------------------------    ---------    -----------------------------");
+        System.out.println("I was made by Ahmed Mabrouk SW testing engineer from the IoT _Vois Egypt");
+        System.out.println("-------------------------    ---------    -----------------------------");
+        System.out.println("Ready ???? Lets Start !!! ");
+    }
+}
---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+import features.WelcomeScreen;
+import utils.vars;
+
+import java.util.Scanner;
+
+public class main {
+    Scanner scan = new Scanner(System.in);
+    public static void main(String[] args)
+    {
+        WelcomeScreen.welcome();
+      //  do {
+
+
+       // }while()
+    }
+}
