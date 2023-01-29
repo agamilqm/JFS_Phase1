047e4123f8e96abe6786da45092239307a575af6
A.Mabrouk <a.gamil.qm@gmail.com>
2023-01-29 15:24:21 +0200

    end of Sprint 1

--- src/main/main.java
+++ src/main/main.java
@@ -4,11 +4,11 @@ import utils.vars;
 import java.util.Scanner;
 
 public class main {
-    Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args)
     {
-        WelcomeScreen.welcome();
       //  do {
+        WelcomeScreen.runWelcomeScreen();
 
 
        // }while()
